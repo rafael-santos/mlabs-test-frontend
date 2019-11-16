320289f12ad9614666f150b5bd7210e6670abe8d
Rafael Augusto dos Santos <rafael.agt.santos@gmail.com>
2019-11-15 23:21:52 -0200

    Topbar better responsiveness

--- src/components/header/Topbar.vue
+++ src/components/header/Topbar.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="topbar">
-    <div class="container">
-     <div class="row align-items-center topbar__row">
-        <div class="col text-left">15/11/2019 19:00:00</div>
+    <div class="container px-0 px-lg-3">
+     <div class="row no-gutters align-items-center topbar__row">
+        <div class="col-2 text-left">
+          <span class="d-none d-lg-inline">15/11/2019 19:00:00</span>
+        </div>
         <div class="col text-center">
           <span>Minha conta</span>
           <i class="fas fa-chevron-down ml-2"></i>
         </div>
-        <div class="col text-right">
-          <i class="fas fa-cog topbar__settings-icon"></i>
+        <div class="col-2 text-right">
+          <button class="topbar__settings-button" onclick="void(0);">
+            <i class="fas fa-cog"></i>
+          </button>
         </div>
       </div>
     </div>
@@ -17,7 +21,7 @@
 
 <style lang="scss">
 .topbar {
-  background-color: var(--color-neutral-gray-darkest);
+  background-color: var(--color-neutral-gray-darker);
   color: var(--color-neutral-gray-light);
   font-size: rem(12);
   text-transform: uppercase;
@@ -27,7 +31,17 @@
   min-height: 32px;
 }
 
-.topbar__settings-icon {
+.topbar__settings-button {
+  background-color: transparent;
+  border: 0;
+  color: inherit;
   font-size: rem(16);
+  min-height: 32px;
+  min-width: 42px;
+  text-align: center;
+}
+
+.topbar__settings-button:hover {
+  background-color: var(--color-neutral-gray-darkest);
 }
 </style>
