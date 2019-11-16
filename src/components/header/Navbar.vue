b4dec6b7e969d2da7d6c5ec43f06eb32999bff2e
Rafael Augusto dos Santos <rafael.agt.santos@gmail.com>
2019-11-16 01:32:28 -0200

    Adding NavbarItems component

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -2,20 +2,71 @@
   <div class="navbar">
     <div class="container px-0 px-lg-3">
       <div class="row no-gutters align-items-center">
-      <div class="col-auto pl-3 pl-lg-0">
-        <img class="navbar__logo" src="@/assets/images/mlabs-logo.svg"/>
+        <!-- Logo -->
+        <div class="col-auto pl-3 pl-lg-0">
+          <img class="navbar__logo" src="@/assets/images/mlabs-logo.svg"/>
+        </div>
+        <!-- Mobile menu button -->
+        <div class="col text-right d-lg-none">
+          <button class="navbar__menu-button" onclick="void(0);">
+            <i class="fas fa-bars"></i>
+          </button>
+        </div>
+        <!-- Desktop menu items -->
+        <NavbarItem
+          to='/'
+          label='Dashboard'
+          icon='dashboard.svg'
+        />
+        <NavbarItem
+          to='/new'
+          label='Agendar Post'
+          icon='schedule.svg'
+        />
+        <NavbarItem
+          to='/schedules'
+          label='Calendário'
+          icon='calendar.svg'
+        />
+        <NavbarItem
+          to='/inbox'
+          label='Inbox'
+          icon='inbox.svg'
+        />
+        <NavbarItem
+          to='/feed'
+          label='Feed'
+          icon='feed.svg'
+        />
+        <NavbarItem
+          to='/workflow'
+          label='Workflow'
+          icon='workflow.svg'
+        />
+        <NavbarItem
+          to='/monitoring'
+          label='Acompanhamento'
+          icon='monitoring.svg'
+        />
+        <NavbarItem
+          to='/reports'
+          label='Relatórios'
+          icon='reports.svg'
+        />
       </div>
-      <div class="col text-right d-lg-none">
-        <button class="navbar__menu-button" onclick="void(0);">
-          <i class="fas fa-bars"></i>
-        </button>
-      </div>
-    </div>
-    <!-- <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> -->
     </div>
   </div>
 </template>
+
+<script>
+import NavbarItem from '@/components/header/NavbarItem.vue';
+
+export default {
+  components: {
+    NavbarItem
+  }
+};
+</script>
 
 <style lang="scss">
 .navbar {
@@ -25,7 +76,7 @@
 
 .navbar__logo {
   @include all-ease;
-  
+
   height: 40px;
   margin: 10px 0;
 }
---
+++ src/components/header/NavbarItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <router-link :to="`${to}`" active-class="navbar-item--active" class="navbar-item">
+    <div class="navbar-item__icon">
+      <inline-svg :src="require(`@/assets/images/menu/${icon}`)" />
+    </div>
+    <div class="navbar-item__label">{{ label }}</div>
+  </router-link>
+  <!-- <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>-->
+</template>
+
+<script>
+import InlineSvg from 'vue-inline-svg';
+
+export default {
+  props: {
+    to: String,
+    label: String,
+    icon: String
+  },
+  components: {
+    InlineSvg
+  }
+};
+</script>
+
+<style lang="scss">
+.navbar-item {
+  color: inherit;
+  padding: 15px 18px;
+  text-align: center;
+  text-decoration: none;
+}
+
+.navbar-item--active {
+  background-color: var(--color-neutral-gray-lighter);
+}
+
+.navbar-item__icon {
+  color: var(--color-neutral-gray-dark);
+}
+
+.navbar-item--active .navbar-item__icon {
+  color: var(--color-neutral-gray-darker);
+}
+</style>
