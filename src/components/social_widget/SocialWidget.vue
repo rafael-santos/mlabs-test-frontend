97a2f466eff9c093a22004178aebec062059f4bb
Rafael Augusto dos Santos <rafael.agt.santos@gmail.com>
2019-11-16 23:17:20 -0200

    Dashboard column layout for social widgets widget

---
+++ src/components/social_widget/SocialWidget.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="social-widget">
+    Widget
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.social-widget {
+  background-color: var(--color-neutral-gray-lighter);
+  border: 2px dotted var(--color-neutral-gray-light);
+  margin: -1px;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="dashboard-view-container">
-    <h1>Dashboard</h1>
+    <div class="container">
+        <div class="row no-gutters align-items-stretch">
+        <template v-for="(channel, index) in socialNetworks">
+          <div :key="index" class="col-md-4 col-lg-3 col-xl social-widget-col">
+            <SocialWidget />
+          </div>
+          <div :key="index" v-if="(index + 1) % 5 === 0" class="w-100 d-none d-xl-block"></div>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import SocialWidget from '@/components/social_widget/SocialWidget.vue';
+
+export default {
+  components: {
+    SocialWidget
+  },
+  data() {
+    return {
+      socialNetworks: [
+        { channelKey: 'facebook', name: 'Facebook' },
+        { channelKey: 'twitter', name: 'Twitter' },
+        { channelKey: 'instagram', name: 'Instagram' },
+        { channelKey: 'google_my_business', name: 'Google meu negócio' },
+        { channelKey: 'pinterest', name: 'Pinterest' },
+        { channelKey: 'linkedin', name: 'Linkedin' },
+        { channelKey: 'youtube', name: 'Youtube' },
+        { channelKey: 'whatsapp', name: 'WhatsApp' },
+        { channelKey: 'whatsapp', name: 'WhatsApp' },
+        { channelKey: 'whatsapp', name: 'WhatsApp' },
+        { channelKey: 'whatsapp', name: 'WhatsApp' },
+        { channelKey: 'whatsapp', name: 'WhatsApp' },
+        { channelKey: 'whatsapp', name: 'WhatsApp' },
+        { channelKey: 'google_analytics', name: 'Google Analytics' }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard-view-container {
+  margin: 40px 0 80px 0;
+}
+
+.social-widget-col {
+  @media (min-width: $screen-lg-min) {
+    max-width: 20%;
+  }
+}
+</style>
